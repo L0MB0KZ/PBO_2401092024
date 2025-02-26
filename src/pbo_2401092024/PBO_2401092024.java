64b4a5a5551bf49f54d2a73a7663ac3ee4ee744d
LABP1KOMP <LABP1KOMP@LAB-P1-PC17>
2025-02-26 17:14:31 +0700

    PBO

---
+++ src/pbo_2401092024/PBO_2401092024.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2401092024;
+
+/**
+ *
+ * @author Pongo
+ */
+public class PBO_2401092024 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Belajar Java");
+        System.out.println("Pemrograman Berorientasi Objek");
+    }
+}
